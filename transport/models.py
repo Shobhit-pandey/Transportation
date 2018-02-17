ad923e89af961f3da91100a5f58f4a458d3bc43a
Shobhit Pandey <Shobhitpandey8@gmail.com>
2018-02-17 17:42:43 +0530

    removed unneccessary stuff

--- transport/models.py
+++ transport/models.py
@@ -5,7 +5,7 @@ class TruckLists(models.Model):
     truck_number = models.CharField(max_length=10, null=False, blank=False, help_text="MH02VG1111")
     insurance = models.DateField(blank=False, null=False, help_text="insurance expiry date(YYYY-MM-DD)")
     fitness = models.DateField(blank=False, null=False, help_text="Fitness expiry date(YYYY-MM-DD)")
-    pollution = models.DateField(blank=False, null=False, help_text="insurance expiry date(YYYY-MM-DD)")
+    pollution = models.DateField(blank=False, null=False, help_text="pollution expiry date(YYYY-MM-DD)")
 
 
 class NotificationInsurance(models.Model):
